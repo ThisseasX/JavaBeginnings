a6448d30da11db049c0afcc6a1488f0001e78dca
ThisseasX <thisseasx@yahoo.com>
2017-11-29 21:40:46 +0200

    Added Experiments and Exercises

---
+++ src/com/thisseasx/november2017/exercises/BookExercise.java
@@ -0,0 +1,18 @@
+package com.thisseasx.november2017.exercises;
+
+public class BookExercise {
+    public static void main(String[] args) {
+        Author jkRowling = new Author(
+                "J.K.Rowling",
+                "jkrowling@gmail.com",
+                "female");
+
+        Book harryPotter = new Book(
+                "Harry Potter",
+                jkRowling,
+                100,
+                9999);
+
+        System.out.println(harryPotter.toString());
+    }
+}
