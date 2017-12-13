03bf52616b6437a8481d6aec8f99c2a55b71bc9f
ThisseasX <thisseasx@yahoo.com>
2017-12-13 22:03:34 +0200

    Added Exercises

---
+++ src/thisseasx/december2017/experiments/TestingTest/TestingTest.java
@@ -0,0 +1,10 @@
+package thisseasx.december2017.experiments.TestingTest;
+
+public class TestingTest {
+
+    public static void main(String[] args) {
+        MathClass mc = new MathClass();
+        System.out.println(mc.checkSign(3));
+        mc.divide(3,2);
+    }
+}
