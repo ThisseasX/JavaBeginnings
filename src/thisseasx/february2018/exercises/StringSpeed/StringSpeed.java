4020817b2271ff47df0157e78fb173503b665907
ThisseasX <thisseasx@yahoo.com>
2018-02-02 20:58:10 +0200

    Added Exercises

---
+++ src/thisseasx/february2018/exercises/StringSpeed/StringSpeed.java
@@ -0,0 +1,46 @@
+package thisseasx.february2018.exercises.StringSpeed;
+
+public class StringSpeed {
+
+    private static final int TIMES = 5000;
+    private static final String SAMPLE = "Hello World";
+
+    public static void main(String[] args) {
+        stringConcat();
+        stringConcatReverse();
+        stringBuild();
+    }
+
+    private static void stringConcat() {
+        long startTime = System.nanoTime();
+        String s = "Hello World";
+
+        for (int i = 0; i < TIMES; i++) s += SAMPLE;
+
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.printf("Elapsed Time with Concat: %s%n", elapsedTime);
+    }
+
+    private static void stringConcatReverse() {
+        long startTime = System.nanoTime();
+        String s = "Hello World";
+
+        for (int times = TIMES; times > 0; times--) s += SAMPLE;
+
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.printf("Elapsed Time with ConcatReverse: %s%n", elapsedTime);
+    }
+
+    private static void stringBuild() {
+        long startTime = System.nanoTime();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < TIMES; i++) {
+            sb.append("Hello World");
+        }
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.printf("Elapsed Time with Build: %s%n", elapsedTime);
+    }
+}
